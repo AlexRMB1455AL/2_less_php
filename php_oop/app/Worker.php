c250bc2671d453635d45d5f97d78e802a9579d19
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-05 15:12:16 +0400

    Create index.php

--- php_oop/app/Worker.php
+++ php_oop/app/Worker.php
@@ -7,8 +7,17 @@ class Worker
         // Строки
         // Числа/Флоат
         // Булевые значения
+        // Null
         // Массивы
+
+    public string $name;
+    public int $age;
+    public array $hours;
 
     // Динамичные значения
         // Функции
+    public function work(): void
+    {
+        print_r('Im working');
+    }
 }
